e847a7ab18ded2709c7578b88fef4ab66deb5a4d
maratsoftware <marat.software@gmail.com>
2026-02-18 19:27:08 +0300

    Разработаны страницы игры и финала, добавлена анимация для кнопок

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,22 @@
 "use strict"
 
 function startGame() {
-	console.log("start game");
+  document.querySelector('.menu').style.display = 'none';
+  document.querySelector('.final').style.display = 'flex';
+  
+  const canvas = document.getElementById('gameCanvas');
+  const ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = '#0070e8';
+  ctx.fillRect(50, 50, 100, 100);
+}
+
+function backToMenu() {
+  document.querySelector('.menu').style.display = 'flex';
+  document.querySelector('.final').style.display = 'none';
+}
+
+window.onload = function() {
+  document.querySelector('.menu').style.display = 'flex';
+  document.querySelector('.game').style.display = 'none';
 }
